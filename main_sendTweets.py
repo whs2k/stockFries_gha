54414023734638cd4465b81111c7703d091939d9
Will Solomon <whs2k@virginia.edu>
2024-01-27 12:10:32 -0800

    Adding try/except for sending tweets

--- main_sendTweets.py
+++ main_sendTweets.py
@@ -1,12 +1,19 @@
 import sys
 import helper
+import traceback
 
 print('num of arguments: ', len(sys.argv))
-print(sys.argv)
+#print(sys.argv)
+
 
 input_args = sys.argv
-print(input_args[1])
-helper.send_tweet(tweet_text='Automated Test Tweet',
-		consumer_key_=str(input_args[1]), consumer_secret_=str(input_args[2]),
-		access_token_=str(input_args[3]), access_token_secret_=str(input_args[4]))
+#print(input_args[1])
+try:
+	helper.send_tweet(tweet_text='Automated Test Tweet',
+			consumer_key_=str(input_args[1]), consumer_secret_=str(input_args[2]),
+			access_token_=str(input_args[3]), access_token_secret_=str(input_args[4]))
+except:
+	print('Error in Sending Tweets: ')
+	traceback.print_exc()
+
 
